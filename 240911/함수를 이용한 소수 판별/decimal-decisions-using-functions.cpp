e5a61a1fadfdb9a48bc79eec23fea6674498155d
chonsa29 <57586218+chonsa29@users.noreply.github.com>
2024-09-11 23:26:46 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 240911/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
 
-bool prime(int a){
+bool prime(int a) {
     int cnt = 0;
-    for(int i =2;i<a;i++){
-        if(a%i == 0) cnt++;
+
+    if (a == 1) return false;
+    else {
+        for (int i = 2;i < a;i++) {
+            if (a % i == 0) cnt++;
+        }
     }
-    if (cnt ==0) return true;
+    if (cnt == 0) return true;
     else return false;
 }
 
@@ -14,11 +18,11 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
 
-    cin >> a >>b ;
+    cin >> a >> b;
 
     int sum = 0;
-    for (int i =a;i<=b;i++){
-        if(prime(i)==1) sum = sum+i;
+    for (int i = a;i <= b;i++) {
+        if (prime(i) == 1) sum = sum + i;
     }
 
     cout << sum;
