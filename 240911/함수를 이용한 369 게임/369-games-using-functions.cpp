a7ea427039e69cb0b2b1d9fdc7ddd135142528f3
chonsa29 <57586218+chonsa29@users.noreply.github.com>
2024-09-11 22:55:11 +0900

    Title: 함수를 이용한 369 게임, Time: 11ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240911/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 240911/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 
 bool find(int a) {
-    int one = a % 10;
-    int ten = a / 10;
-
+    int one = a % 10; //1
+    int ten = a / 10; //10
+    int c = a % 100; //100
+    int d = a / 100; //1000
+    int e = a % 1000; //10000
+    int f = a / 1000; //100000
+    int g = a % 10000;
+    int h = a / 10000;
+    int i = a % 100000;
     if (one == 3 || one == 6 || one == 9) return true;
     else if (ten == 3 || ten == 6 || ten == 9) return true;
+    else if (c == 3 || c == 6 || c == 9) return true;
+    else if (d == 3 || d == 6 || d == 9) return true;
+    else if (e == 3 || e == 6 || e == 9) return true;
+    else if (f == 3 || f == 6 || f == 9) return true;
+    else if (g == 3 || g == 6 || g == 9) return true;
+    else if (h == 3 || h == 6 || h == 9) return true;
+    else if (i == 3 || i == 6 || i == 9) return true;
     else return false;
 }
 
