3939462ff1a0e991691e6551abe1791807e1d3ff
chonsa29 <57586218+chonsa29@users.noreply.github.com>
2024-09-11 23:00:01 +0900

    Title: 함수를 이용한 369 게임, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240911/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 240911/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -1,30 +1,14 @@
 #include <iostream>
 using namespace std;
 
-bool find(int a) {
-    int one = a % 10; //1
-    int ten = a / 10; //10
-    int c = a % 100; //100
-    int d = a / 100; //1000
-    int e = a % 1000; //10000
-    int f = a / 1000; //100000
-    int g = a % 10000;
-    int h = a / 10000;
-    int i = a % 100000;
-    int j = a / 100000;
-    int k = a % 1000000;
-    if (one == 3 || one == 6 || one == 9) return true;
-    else if (ten == 3 || ten == 6 || ten == 9) return true;
-    else if (c == 3 || c == 6 || c == 9) return true;
-    else if (d == 3 || d == 6 || d == 9) return true;
-    else if (e == 3 || e == 6 || e == 9) return true;
-    else if (f == 3 || f == 6 || f == 9) return true;
-    else if (g == 3 || g == 6 || g == 9) return true;
-    else if (h == 3 || h == 6 || h == 9) return true;
-    else if (i == 3 || i == 6 || i == 9) return true;
-    else if (j == 3 || j == 6 || j == 9) return true;
-    else if (k == 3 || k == 6 || k == 9) return true;
-    else return false;
+bool find(int a) { 
+    while(n >0){
+        if (n%10 == 3 || n%10 ==6 || n%10 ==9)
+        return true;
+        n /=10;
+    }
+
+    return false;
 }
 
 bool mul(int a) {
