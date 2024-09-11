2d7fcffff9719aa1b0e12dc8a2ec31965a4806a0
chonsa29 <57586218+chonsa29@users.noreply.github.com>
2024-09-11 22:04:53 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240911/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void print(){
+    cout << "**********" << endl;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for(int i =0;i<5;i++){
+        print();
+    }
+    return 0;
+}
